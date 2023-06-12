17a7bd430a470283104e4b797225f518fa9d77a3
jacobsqxv <agyeijacobs@outlook.com>
2023-06-12 14:52:34 +0000

    static files

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// Menu
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownButton = document.querySelector(".dropdown-button");
+
+if (dropdownButton) {
+  dropdownButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
+  });
+}
